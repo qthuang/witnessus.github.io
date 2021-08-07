af5884ddd3642b3d853515d53e6523730a453bb7
zhuwanyun <71127011+zhuwanyun@users.noreply.github.com>
2021-08-07 23:42:28 +0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,41 @@
+var ec_map = echarts.init(document.getElementById("map"));
+var ec_option={
+    title:{
+        text:"",
+    },
+    tooltip:{
+        tirgger:"item",
+        formatter:"省份名 : {b} <br /> 请求人数 : {c} 人",
+    },
+    visualMap:{
+        x:"right",
+        y:"bottom",
+        splitList:[
+            {start:0,end:0},
+            {start:1,end:49},
+            {start:50,end:99},
+            {start:100,end:149},
+            {start:150}
+        ],
+    },
+    series:[{
+        type:"map",
+        mapType:"china",
+        data:null,
+        label:{
+            normal:{
+                show:true,//显示省份名称
+                fontSize:10,
+            },
+        },
+    }]
+};
+$.ajax({
+    type:"get",
+    url:"area.json",
+    async:false,
+    success:function(data){
+        ec_option.series[0].data=data.data;
+    }
+});
+ec_map.setOption(ec_option);
